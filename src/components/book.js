037916f7079404450b62c158ab43b6c1101329fc
Leslie <Kooliesk@gmail.com>
2023-04-08 09:28:55 +0200

    Make changes required by the code reviewer

--- src/components/book.js
+++ src/components/book.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { removeBook } from '../redux/books/booksSlice';
+import ProgressBar from './progress';
 
 const Book = ({ book }) => {
   const dispatch = useDispatch();
@@ -14,15 +15,49 @@ const Book = ({ book }) => {
         {' '}
         <strong className="author">{book.author}</strong>
       </p>
-      <button
-        type="button"
-        onClick={() => {
-          dispatch(removeBook(book.id));
-        }}
-        className="Remove"
-      >
-        Remove
-      </button>
+      <ul className="btn-container">
+        <li>
+          <button className="Comments Remove" type="button">
+            Comments
+          </button>
+        </li>
+        <div className="Line-2" />
+        <li>
+          <button
+            type="button"
+            onClick={() => {
+              dispatch(removeBook(book.id));
+            }}
+            className="Remove"
+          >
+            Remove
+          </button>
+        </li>
+        <div className="Line-2" />
+        <li>
+          <button className="Edit Remove" type="button">
+            Edit
+          </button>
+        </li>
+      </ul>
+      <div className="book-progress">
+        <ProgressBar />
+        <div className="progress-text">
+          <p className="Percent-Complete">
+            {Math.floor(Math.random() * (99 - 0)) + 0}
+            <span>%</span>
+          </p>
+          <p className="Completed">Completed</p>
+        </div>
+        <hr className="vertical-line" />
+        <div className="chapters">
+          <p className="Current-Chapter">CURRENT CHAPTER</p>
+          <p className="Current-Lesson">Chapter 12</p>
+          <button type="button" className="Update-progress">
+            UPDATE PROGRESS
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/progress.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+// eslint-disable-next-line jsx-a11y/control-has-associated-label
+const ProgressBar = () => <div role="progressbar" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100" />;
+
+export default ProgressBar;
